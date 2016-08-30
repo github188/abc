c12b4d7bcf1815f89c61ecef579fc31e336f3ef1
墨鱼拉面 <304025986@QQ.COM>
2016-08-30 17:09:18 +0800

    大体结构如上

--- 14-项目开发/01-平台开发/01-源代码/trunk/JDP/src/com/jd/pims/pem/controller/AppController.java
+++ 14-项目开发/01-平台开发/01-源代码/trunk/JDP/src/com/jd/pims/pem/controller/AppController.java
@@ -1,15 +1,95 @@
 package com.jd.pims.pem.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/as")
+@RequestMapping("/jdemws")
 public class AppController {
 	@Value("${app.address}")  
     private String appAddress;
-
+	
+	/**
+	 * 业务接口
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	@ResponseBody
+	public String login(HttpServletRequest request){
+		return "SUCCESS";
+	}
+	
+	/**
+	 * 业务接口
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/getNumberOnDuty", method = RequestMethod.POST)
+	@ResponseBody
+	public String getNumberOnDuty(HttpServletRequest request){
+		return "SUCCESS";
+	}
+	
+	/**
+	 * 业务接口
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/getEfficiency", method = RequestMethod.POST)
+	@ResponseBody
+	public String getEfficiency(HttpServletRequest request){
+		return "SUCCESS";
+	}
+	
+	/**
+	 * 业务接口
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/getNumberHistory", method = RequestMethod.POST)
+	@ResponseBody
+	public String getNumberHistory(HttpServletRequest request){
+		return "SUCCESS";
+	}
+	
+	/**
+	 * 业务接口
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/getEfficiencyHistory", method = RequestMethod.POST)
+	@ResponseBody
+	public String getEfficiencyHistory(HttpServletRequest request){
+		return "SUCCESS";
+	}
+	
+	/**
+	 * 业务接口
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/getOrganization", method = RequestMethod.POST)
+	@ResponseBody
+	public String getOrganization(HttpServletRequest request){
+		return "SUCCESS";
+	}
+	
+	/**
+	 * 业务接口
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/verifyAccount", method = RequestMethod.POST)
+	@ResponseBody
+	public String verifyAccount(HttpServletRequest request){
+		return "SUCCESS";
+	}
 	
 	public String getAppAddress() {
 		return appAddress;
@@ -18,5 +98,4 @@ public class AppController {
 	public void setAppAddress(String appAddress) {
 		this.appAddress = appAddress;
 	} 
-	
 }
