996a1e167850fc3afecb01e141d2e0530d99b9c7
habibliu <740033486@qq.com>
2016-09-01 17:47:45 +0800

    增加了一getter,setter方法

--- 14-项目开发/01-平台开发/01-源代码/trunk/JDP/src/com/jd/pims/user/model/User.java
+++ 14-项目开发/01-平台开发/01-源代码/trunk/JDP/src/com/jd/pims/user/model/User.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 
+import com.google.gson.Gson;
+
 /**
  * 用户领域对象
  */
@@ -23,10 +25,68 @@ public class User implements Serializable{
 	private String status;
 	private String controlunitid;
 
-	public String toString() {
-		return "id:" + id + ",userName:" + userName + ",personId:" + personId
-				+ ",userType:" + userType + ",status:" + status
-				+ ",controlunitid:" + controlunitid;
+	
+
+	public String getId() {
+		return id;
 	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPersonId() {
+		return personId;
+	}
+
+	public void setPersonId(String personId) {
+		this.personId = personId;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getControlunitid() {
+		return controlunitid;
+	}
+
+	public void setControlunitid(String controlunitid) {
+		this.controlunitid = controlunitid;
+	}
+
+	/**
+	 * 转成json字符串数组
+	 */
+	public String toString() {
+		return new Gson().toJson(this);
+	}
 }
