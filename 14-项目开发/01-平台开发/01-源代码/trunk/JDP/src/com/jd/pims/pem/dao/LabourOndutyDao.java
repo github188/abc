1fd167cab622b044a9e1a8102cae308de4712312
huozhuangning <947837903@qq.com>
2016-09-06 17:05:05 +0800

    提交

--- 14-项目开发/01-平台开发/01-源代码/trunk/JDP/src/com/jd/pims/pem/dao/LabourOndutyDao.java
+++ 14-项目开发/01-平台开发/01-源代码/trunk/JDP/src/com/jd/pims/pem/dao/LabourOndutyDao.java
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-package com.jd.pims.pem.dao;
-
-import java.util.LinkedList;
-
-import com.jd.pims.comm.IBaseDao;
-import com.jd.pims.pem.model.LabourOndutyState;
-
-public interface LabourOndutyDao extends IBaseDao{
-
-	LinkedList<LabourOndutyState> getNumberOnDuty(String cuId);
-
-}
-=======
 package com.jd.pims.pem.dao;
 
 import com.jd.pims.comm.IBaseDao;
@@ -19,4 +5,3 @@ import com.jd.pims.comm.IBaseDao;
 public interface LabourOndutyDao extends IBaseDao{
 
 }
->>>>>>> d732debe3864b9f328caa8e45b80d5f7bf5cb4bb
