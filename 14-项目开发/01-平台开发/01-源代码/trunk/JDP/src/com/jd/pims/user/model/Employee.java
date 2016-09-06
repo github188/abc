1c2bd1d2b6f888dad9aced543f5c9719784030ca
habibliu <740033486@qq.com>
2016-09-06 15:09:53 +0800

    修改员工属性

--- 14-项目开发/01-平台开发/01-源代码/trunk/JDP/src/com/jd/pims/user/model/Employee.java
+++ 14-项目开发/01-平台开发/01-源代码/trunk/JDP/src/com/jd/pims/user/model/Employee.java
@@ -1,6 +1,5 @@
 package com.jd.pims.user.model;
 
-import java.io.Serializable;
 
 import javax.persistence.Entity;
 
@@ -16,9 +15,21 @@ public class Employee extends BaseDataModel{
 
 	private String id;
 	
+	private String personCode;
+	
 	private String personName;
 	
 	private String sex;
+	
+	private Integer age;
+	
+	private String telephone;//手机
+	
+	private String officeTel;//办公电话
+	
+	private String address;
+	
+	private String nativePlace;//户籍
 	
 	private String orgId;
 
@@ -32,77 +43,188 @@ public class Employee extends BaseDataModel{
 	
 	private String cuName;
 
+	
+
+
 	public String getId() {
 		return id;
 	}
+
+
 
 	public void setId(String id) {
 		this.id = id;
 	}
 
+
+
+	public String getPersonCode() {
+		return personCode;
+	}
+
+
+
+	public void setPersonCode(String personCode) {
+		this.personCode = personCode;
+	}
+
+
+
 	public String getPersonName() {
 		return personName;
 	}
+
+
 
 	public void setPersonName(String personName) {
 		this.personName = personName;
 	}
 
+
+
 	public String getSex() {
 		return sex;
 	}
+
+
 
 	public void setSex(String sex) {
 		this.sex = sex;
 	}
 
+
+
+	public Integer getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+
+
+	public String getOfficeTel() {
+		return officeTel;
+	}
+
+
+
+	public void setOfficeTel(String officeTel) {
+		this.officeTel = officeTel;
+	}
+
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+
+	public String getNativePlace() {
+		return nativePlace;
+	}
+
+
+
+	public void setNativePlace(String nativePlace) {
+		this.nativePlace = nativePlace;
+	}
+
+
+
 	public String getOrgId() {
 		return orgId;
 	}
+
+
 
 	public void setOrgId(String orgId) {
 		this.orgId = orgId;
 	}
 
+
+
 	public String getOrgCode() {
 		return orgCode;
 	}
+
+
 
 	public void setOrgCode(String orgCode) {
 		this.orgCode = orgCode;
 	}
 
+
+
 	public String getOrgName() {
 		return orgName;
 	}
+
+
 
 	public void setOrgName(String orgName) {
 		this.orgName = orgName;
 	}
 
+
+
 	public String getCuId() {
 		return cuId;
 	}
+
+
 
 	public void setCuId(String cuId) {
 		this.cuId = cuId;
 	}
 
+
+
 	public String getCuCode() {
 		return cuCode;
 	}
+
+
 
 	public void setCuCode(String cuCode) {
 		this.cuCode = cuCode;
 	}
 
+
+
 	public String getCuName() {
 		return cuName;
 	}
 
+
+
 	public void setCuName(String cuName) {
 		this.cuName = cuName;
 	}
+	
+
 	
 	/**
 	 * 转成json字符串
@@ -110,4 +232,5 @@ public class Employee extends BaseDataModel{
 	public String toString(){
 		return new Gson().toJson(this);
 	}
+
 }
