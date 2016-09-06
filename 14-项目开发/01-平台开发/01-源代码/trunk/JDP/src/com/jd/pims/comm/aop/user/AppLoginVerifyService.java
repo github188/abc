d732debe3864b9f328caa8e45b80d5f7bf5cb4bb
habibliu <740033486@qq.com>
2016-09-06 16:42:01 +0800

    实现访问拦截

--- 14-项目开发/01-平台开发/01-源代码/trunk/JDP/src/com/jd/pims/comm/aop/user/AppLoginVerifyService.java
+++ 14-项目开发/01-平台开发/01-源代码/trunk/JDP/src/com/jd/pims/comm/aop/user/AppLoginVerifyService.java
@@ -4,15 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletWebRequest;
 
 import com.google.gson.JsonObject;
 import com.jd.pims.comm.LoginInfo;
@@ -23,29 +18,32 @@ public class AppLoginVerifyService {
 	private static final Logger logger = Logger
 			.getLogger(AppLoginVerifyService.class.getName());
 
-	
-	//@Before("@annotation(com.jd.pims.comm.aop.user.Verify)")
-	//@Around("execution(* com.jd.pims.pem.controller.*.*(..))")
-	//@Around(value="@annotation(com.jd.pims.comm.aop.user.Verify)")
-	 @Around("execution(* com.jd.pims.pem.controller.AppController.* (..))") //all  
-	public Object  process(ProceedingJoinPoint joinPoint)
-			throws Throwable {
-		ServletWebRequest servletContainer = (ServletWebRequest) RequestContextHolder
-				.getRequestAttributes();
+	@Around("execution(* com.jd.pims.pem.controller.*.* (..))")
+	public Object process(ProceedingJoinPoint joinPoint) throws Throwable {
 
-		HttpServletRequest request = servletContainer.getRequest();
-
-		String empId = request.getParameter("empId");
-		if (empId == null||!LoginInfo.isLogin(empId)) {
-			String tmp = joinPoint.getSignature().toString();
-			logger.debug("您没有得到授权！" + tmp);
-			JsonObject retMsg = new JsonObject();
-			retMsg.addProperty("returnCode", -1);
-			retMsg.addProperty("message", "您没有得到授权！");
-			HttpServletResponse response = servletContainer.getResponse();
-			response.getWriter().print(retMsg.toString());
-			return null;
+		logger.debug("拦截方法,判断是否登录!");
+		
+		Object[] args = joinPoint.getArgs();
+		HttpServletRequest request = null;
+		for (Object param : args) {
+			if (param instanceof HttpServletRequest) {
+				request = (HttpServletRequest) param;
+				break;
+			}
+			
 		}
+		if (request != null ) {
+			String empId = request.getParameter("empId");
+			if (empId == null || !LoginInfo.isLogin(empId)) {
+				String tmp = joinPoint.getSignature().toString();
+				logger.debug("您没有得到授权！" + tmp);
+				JsonObject retMsg = new JsonObject();
+				retMsg.addProperty("returnCode", -1);
+				retMsg.addProperty("message", "您没有得到授权！");
+				return retMsg.toString();
+			}
+		}
+
 		Object object = joinPoint.proceed();
 		return object;
 	}
