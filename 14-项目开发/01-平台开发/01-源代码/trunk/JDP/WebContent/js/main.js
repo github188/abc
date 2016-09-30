f12f8b560eb99f1f741871e3242deb0d24427fc3
yang3350380 <304025986@qq.com>
2016-09-30 11:36:17 +0800

    no message

--- 14-项目开发/01-平台开发/01-源代码/trunk/JDP/WebContent/js/main.js
+++ 14-项目开发/01-平台开发/01-源代码/trunk/JDP/WebContent/js/main.js
@@ -968,12 +968,12 @@ option = {
 				$.each(data, function(index, row){
 					//name 地区名 averageEffect 该地区平均人效
 					bardata2[0].push(row.name);
-					bardata2[1].push((row.NotEmpNum+row.EmpNum)>0?eval(row.orderNum/(row.NotEmpNum+row.EmpNum)).toFixed(2):0);
+					bardata2[1].push((row.NotEmpNum+row.EmpNum)>0?Math.ceil(eval(row.orderNum/(row.NotEmpNum+row.EmpNum))):0);
 					totalClerkNum += (row.NotEmpNum+row.EmpNum);
 					totalOrderNum += row.orderNum;
 				});
 			}
-			setBar2Option(bardata2,((data&&data.length>0&&totalClerkNum>0)?(totalOrderNum/totalClerkNum).toFixed(2):'0'),totalOrderNum,name);
+			setBar2Option(bardata2,((data&&data.length>0&&totalClerkNum>0)?Math.ceil(totalOrderNum/totalClerkNum):'0'),totalOrderNum,name);
 		}
 
 		function setBar2Option(bardata2,avgEffect,totalOrderNum,name){
