7e4736564a0b211f6c59e5cc8ca648ec2e7ca384
huozhuangning <947837903@qq.com>
2016-10-27 17:54:50 +0800

    墨鱼拉面提交 貌似取时间逻辑有点问题

--- 14-项目开发/01-平台开发/01-源代码/trunk/JDP/src/com/jd/pims/pem/service/impl/ChartServiceImpl.java
+++ 14-项目开发/01-平台开发/01-源代码/trunk/JDP/src/com/jd/pims/pem/service/impl/ChartServiceImpl.java
@@ -50,7 +50,7 @@ public class ChartServiceImpl implements IChartService {
 		SimpleDateFormat df = new SimpleDateFormat("HH:mm:00");
 		calendar.set(Calendar.MINUTE, calendar.get(Calendar.MINUTE));
 		String end= df.format(calendar.getTime());
-		calendar.set(Calendar.MINUTE, calendar.get(Calendar.MINUTE) -20);
+		calendar.add(Calendar.MINUTE, -20);
 		String begin = df.format(calendar.getTime());
 		List<Map<String,Object>> arealist=userDao.getCurrentTimeAreaForChart(name);
 		for(Map<String,Object>map:arealist){
